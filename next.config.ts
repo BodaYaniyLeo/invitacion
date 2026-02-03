7e666c0a908cbb468841e005ca2298148ac85647
Ezeiac <ezearena@gmail.com>
2026-02-03 15:49:39 +0100

    config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/invitacion',
-  assetPrefix: '/invitacion',
-  trailingSlash: true,
+
 };
 
 export default nextConfig;
