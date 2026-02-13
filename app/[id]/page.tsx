b7874487414e4b9e2cf36721d452528125b9622c
Ezeiac <ezearena@gmail.com>
2026-02-13 20:15:35 +0100

    prueba

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,30 +1,23 @@
-import { createClient } from '@/app/lib/supabase'
-import { Invitation } from '../src/components/Invitation'
+import { createServerSupabaseClient } from "@/app/lib/supabase/server"
+import { Invitation } from "../src/components/Invitation"
 
-export const dynamic = "force-dynamic"
 
 export default async function Page({
     params,
 }: {
-    params: Promise<{ id: string }>
+    params: { id: string }
 }) {
 
-    const { id } = await params
-
-    const supabase = createClient()
+    const supabase = createServerSupabaseClient()
 
     const { data, error } = await supabase
-        .from('guests')
-        .select('*')
-        .eq('slug', id)
+        .from("guests")
+        .select("*")
+        .eq("slug", params.id)
 
     if (error || !data) {
         return <div>Grupo no encontrado</div>
     }
 
-    return (
-        <div>
-            <Invitation />
-        </div>
-    )
+    return <Invitation />
 }
