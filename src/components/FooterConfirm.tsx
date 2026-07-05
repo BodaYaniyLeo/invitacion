a2bcdeb55c784ae02d2c6dfe9cc8da73a4b2be5d
Ezeiac <ezearena@gmail.com>
2026-07-05 21:08:03 +0200

    mejoras asist

--- src/components/FooterConfirm.tsx
+++ src/components/FooterConfirm.tsx
@@ -40,7 +40,7 @@ export const FooterConfirm = ({
     }, []);
 
     return (
-        <div id="containerConfirm" ref={containerRef} className="px-6 pointer-events-auto py-2 justify-items-center w-full bg-[#111117] content-center mb-10 min-h-[45dvh] max-w-[1000px] lg:justify-self-center">
+        <div id="containerConfirm" ref={containerRef} className="px-6 pointer-events-auto py-2 justify-items-center w-full bg-[#111117] content-center mb-10 my-5 max-w-[1000px] lg:justify-self-center">
             <div id="confirmData" className="flex flex-col w-full max-w-120 rounded-xl backdrop-blur-md opacity-0 invisible translate-y-4">
                 <p className="title-confirm flex-none pb-4 font-bold text-center uppercase tracking-wider text-(length:--h4size)">
                     Confirmar asistencia
@@ -54,19 +54,21 @@ export const FooterConfirm = ({
                             <div key={g.id} className="mb-4 last:mb-0">
                                 <div className='flex justify-between items-center gap-4'>
                                     <h2 className="guest-name py-2 text-(length:--h5size)">{g.name} {g.lastname}</h2>
-                                    <AnswerComponent
-                                        id={g.id}
-                                        dataGuest={dataGuest}
-                                        setDataGuest={setDataGuest}
-                                        confirm={g.confirm}
-                                        status={"confirm"}
-                                        setVisibleOptions={setVisibleOptions}
-                                    />
-                                    <button onClick={() => setVisibleOptions(prev => prev === g.id ? null : g.id)}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 12 12" className={`min-w-6 duration-500 ${g.confirm ? "opacity-100" : "opacity-10"} ${g.confirm && visibleOptions === g.id ? "-rotate-180" : "rotate-0"}`}>
-                                            <path fill="currentColor" d="M2.22 4.47a.75.75 0 0 1 1.06 0L6 7.19l2.72-2.72a.75.75 0 0 1 1.06 1.06L6.53 8.78a.75.75 0 0 1-1.06 0L2.22 5.53a.75.75 0 0 1 0-1.06" />
-                                        </svg>
-                                    </button>
+                                    <div className='flex'>
+                                        <AnswerComponent
+                                            id={g.id}
+                                            dataGuest={dataGuest}
+                                            setDataGuest={setDataGuest}
+                                            confirm={g.confirm}
+                                            status={"confirm"}
+                                            setVisibleOptions={setVisibleOptions}
+                                        />
+                                        <button onClick={() => setVisibleOptions(prev => prev === g.id ? null : g.id)}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 12 12" className={`min-w-6 duration-500 ms-1 ${g.confirm ? "opacity-100" : "opacity-10"} ${g.confirm && visibleOptions === g.id ? "-rotate-180" : "rotate-0"}`}>
+                                                <path fill="currentColor" d="M2.22 4.47a.75.75 0 0 1 1.06 0L6 7.19l2.72-2.72a.75.75 0 0 1 1.06 1.06L6.53 8.78a.75.75 0 0 1-1.06 0L2.22 5.53a.75.75 0 0 1 0-1.06" />
+                                            </svg>
+                                        </button>
+                                    </div>
                                 </div>
                                 <div className={`flex flex-col justify-between gap-4 mt-2 overflow-hidden duration-500 ${g.confirm && visibleOptions === g.id ? "h-20" : "h-0"}`}>
                                     <InputFood
