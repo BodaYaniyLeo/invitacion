f5da4bcc07f09df98cefe200bfc37f3c2d954288
Ezeiac <ezearena@gmail.com>
2026-07-09 14:00:26 +0200

    cambios admin

--- src/helpers/sendAdminData.ts
+++ src/helpers/sendAdminData.ts
@@ -21,7 +21,7 @@ export const sendAdminData = async ({ guests, setTextButton, setAnimateButton, c
 
         if (hasError) {
             console.log("Hubo un error en alguna de las actualizaciones");
-        } else {
+        } else if (setPendingChanges) {
             setAnimateButton(true)
             setTimeout(() => {
                 setTextButton("Guardado!");
